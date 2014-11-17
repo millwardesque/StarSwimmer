1cc039551651aa89442b9df26a3ec5e01b75bae0
Christopher Millward <christopher.millward@gmail.com>
2014-11-17 12:50:40 -0500

    Initial commit of StarSwimmer.

---
+++ Assets/Scripts/PlayerControlEnhancements.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControlEnhancements : MonoBehaviour {
+	CharacterMotor motor;
+
+	// Use this for initialization
+	void Start () {
+		motor = GetComponent<CharacterMotor>();
+		if (!motor) {
+			Debug.LogError("Failed to start PlayerControlEnhancements: No motor is attached");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.LeftShift)) {
+			motor.movement.maxForwardSpeed *= 2.0f;
+		}
+		else if (Input.GetKeyUp (KeyCode.LeftShift)) {
+			motor.movement.maxForwardSpeed /= 2.0f;
+		}
+
+	}
+}
---
+++ Assets/Scripts/PlayerSFX.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerSFX : MonoBehaviour {
+	public AudioClip walkOnWater;
+	public AudioClip walkOnLand;
+	private AudioSource audioSource;
+
+	// Use this for initialization
+	void Start () {
+		audioSource = GetComponent<AudioSource>();
+		if (!audioSource) {
+			Debug.LogError("Unable to initialize player SFX: No Audio Source attached.");
+		}
+		if (!walkOnWater) {
+			Debug.LogWarning("Player has no walk-on-water sound FX.");
+		}
+		if (!walkOnLand) {
+			Debug.LogWarning("Player has no walk-on-land sound FX.");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnControllerColliderHit(ControllerColliderHit hit) {
+		if (hit.collider.tag == "Water" && walkOnWater) {
+			if (audioSource.clip != walkOnWater) {
+				audioSource.clip = walkOnWater;
+			}
+
+			if (!audioSource.isPlaying) {
+				audioSource.Play();
+			}
+		}
+		else if (hit.collider.tag == "Land" && walkOnLand) {
+			if (audioSource.clip != walkOnLand) {
+				audioSource.clip = walkOnLand;
+			}
+			
+			if (!audioSource.isPlaying) {
+				audioSource.Play();
+			}
+		}
+		else {
+			if (audioSource.isPlaying) {
+				audioSource.Stop();
+			}
+		}
+	}
+}
